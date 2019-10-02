d4a28f7564629a6eda8b0a8a547c92727716288b
gabriel <gabrielcunhadev@gmail.com>
2019-10-01 23:40:11 -0300

    Create AppBar and floatActionButton in contact_page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:agenda_contatos/ui/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(Home());
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: true,
+    title: 'Agenda de Contatos',
+    theme: ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    home: HomePage(title: "Agenda de contatos"),
+  ));
+}
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -4,20 +4,6 @@ import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'Agenda de Contatos',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: HomePage(title: "Agenda de Contatos"),
-    );
-  }
-}
-
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
   final String title;
