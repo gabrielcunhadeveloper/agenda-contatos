8e4656cff17083f0bbdea32868e604ca30665b2d
gabriel <gabrielcunhadev@gmail.com>
2019-10-01 22:53:33 -0300

    Create List builder for contacts list.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -25,22 +27,96 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   ContactHelper helper = ContactHelper();
+  List<Contact> contacts = List();
 
+  @override
+  void initState() {
+    super.initState();
 
+    bool debug = false;
+    if (debug) {
+      Contact c = Contact();
+      c.name = "Gabriel Cunha";
+      c.email = "gabriel@gmail.com";
+      c.phone = "32329999";
+      c.img = "imgTest";
+
+      helper.saveContact(c);
+    }
+
+    helper.getAllContacts().then((list){
+//      print(helper.getAllContacts());
+
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-          ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+              padding: EdgeInsets.all(10.0),
+              itemCount: contacts.length ,
+              itemBuilder: (context, index) {
+                return _contactsCard(context, index);
+              },
+            ),
+    );
+  }
+
+  Widget _contactsCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding (
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null ? FileImage(File(contacts[index].img)) : AssetImage("images/person.png"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      contacts[index].name ?? "",
+                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      contacts[index].email ?? "",
+                      style: TextStyle(fontSize: 16.0),
+
+                    ),
+                    Text(
+                      contacts[index].phone ?? "",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
